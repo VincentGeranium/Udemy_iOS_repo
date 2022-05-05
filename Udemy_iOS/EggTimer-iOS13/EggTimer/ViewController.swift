699069330767206927f38a7d925d4800388f1113
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-05-05 14:13:51 +0900

    feat: Section 8 project.

---
+++ Udemy_iOS/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    var eggTimes = [ "Soft": 3, "Medium": 5, "Hard": 7 ]
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    var player: AVAudioPlayer?
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        guard let hardness = sender.currentTitle else { return }
+        
+        guard let eachEggTimes = eggTimes[hardness] else { return }
+        
+        self.totalTime = eachEggTimes
+        
+        progressView.progress = 0.0
+        
+        secondsPassed = 0
+        
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            
+            progressView.progress = percentageProgress
+            print(percentageProgress)
+            
+            
+        } else {
+            timer.invalidate()
+            
+            titleLabel.text = "DONE!"
+            
+            playSound()
+        }
+    }
+    
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
+
