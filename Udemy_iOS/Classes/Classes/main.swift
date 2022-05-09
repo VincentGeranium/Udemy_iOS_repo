480229d4b1ce85d59b94d01ea8f648c7faf44104
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-05-10 07:46:35 +0900

    feat, fix:  [Swift Deep Dive] Structs v.s. Classes

--- Udemy_iOS/Classes/Classes/main.swift
+++ Udemy_iOS/Classes/Classes/main.swift
@@ -1,11 +1,10 @@
 
-let skeleton = Enemy()
+var skeleton1 = Enemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton1
 
+skeleton1.takeDamage(amount: 10)
+skeleton1.takeDamage(amount: 10)
+skeleton2.takeDamage(amount: 10)
 
-let dragon = Dragon()
-dragon.wingSpan = 5
-dragon.attackStrength = 15
-dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane.")
-dragon.move()
-dragon.attack()
-
+print(skeleton1.health)
+print(skeleton2.health)
