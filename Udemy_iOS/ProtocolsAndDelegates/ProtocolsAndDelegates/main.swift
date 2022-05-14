3f06cd90ea98c3ca1f5c181c2a04715052abb99a
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-05-15 06:58:54 +0900

    feat: study about protocol and delegate pattern.

---
+++ Udemy_iOS/ProtocolsAndDelegates/ProtocolsAndDelegates/main.swift
@@ -0,0 +1,59 @@
+
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happend?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic dose chest compressions, 30 per second.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The doctor dose chest compressions, 30 per second.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sounds")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    
+    func useElectricDrill() {
+        print("Whirr...")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let minsung = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
