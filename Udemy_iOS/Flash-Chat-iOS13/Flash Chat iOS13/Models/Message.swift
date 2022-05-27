9ec6a49d8be5f4098c47fe24706c813aa685e170
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-05-27 12:17:04 +0900

    Section 15 project.

---
+++ Udemy_iOS/Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kwangjun Kim on 2022/05/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
