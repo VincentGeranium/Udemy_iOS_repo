f70220d7848007d901b5ddfe64730881aa6444dc
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-06-01 17:55:27 +0900

    added NavigationView and Link.

--- Udemy_iOS/islandCard/islandCard/Views/ContentView.swift
+++ Udemy_iOS/islandCard/islandCard/Views/ContentView.swift
@@ -9,27 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(red: 0.18, green: 0.80, blue: 0.44, opacity: 1.00)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Image("islandSumn")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
-                Text("dear_island.sumn")
-                    .font(Font.custom("Pacifico-Regular", size: 40))
-                    .bold()
-                    .foregroundColor(.white)
-                Text("Artist")
-                    .foregroundColor(.white)
-                    .font(.system(size: 25))
-                Divider()
-                
-                InfoView(text: "+25 01 5816 1020", imageName: "phone.fill")
-                InfoView(text: "4orisland@gmail.com", imageName: "envelope.fill")
+        NavigationView {
+            ZStack {
+                Color(red: 0.18, green: 0.80, blue: 0.44, opacity: 1.00)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Image("islandSumn")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                    Text("dear_island.sumn")
+                        .font(Font.custom("Pacifico-Regular", size: 40))
+                        .bold()
+                        .foregroundColor(.white)
+                    Text("Artist")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25))
+                    Divider()
+                    NavigationLink(destination: LikeView()) {
+                        InfoView(text: "Tap", imageName: "suit.heart.fill")
+                    }
+                }
             }
         }
     }
--- Udemy_iOS/islandCard/islandCard/Views/LikeView.swift
+++ Udemy_iOS/islandCard/islandCard/Views/LikeView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct LikeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.18, green: 0.80, blue: 0.44, opacity: 1.00)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Like")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Like you")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("From Minsung 😝")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                InfoView(text: "+25 01 3952 2228", imageName: "suit.heart.fill")
+                InfoView(text: "Like you", imageName: "suit.heart.fill")
+            }
+        }
     }
 }
 
